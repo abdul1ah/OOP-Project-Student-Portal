0876a93bbf6f0ce165c2a0d79fb2c28b3eb756f5
abdul1ah <ma2695633@gmail.com>
2024-05-04 16:55:18 +0500

    updated admin class

--- Database.hpp
+++ Database.hpp
@@ -11,7 +11,54 @@ using namespace std;
 // Class declaration for Admin
 class Admin
 {
-protected:
-    string username;  // Admin username
-    string password;  // Admin password
+    protected:
+        string username;  // Admin username
+        string password;  // Admin password
+
+    private:
+        // Teacher attributes
+        string t_id;
+        string t_firstname;
+        string t_lastname;
+        string t_department;
+        string tuser_name;
+        string tpassword;
+        string t_date;
+        string t_gender;
+        string t_number;
+        string t_qualification;
+        string address;
+            struct Marks
+        {
+            string s_marks;
+            string s_subject;
+        };
+
+        // Student attributes
+        string suser_name,spassword;
+        string s_id;
+        string s_firstname;
+        string s_lastname;
+        string s_department;
+        string s_user_name;
+        string s_password;
+        string s_date;
+        string s_gender;
+        string s_number;
+        string s_address;
+        string fee;
+        string s_qualification;
+        string s_marks;
+        void setMarks(Marks [], int);
+        string getMarks(Marks [], int);
+
+    public:
+        // Function declarations for Admin operations
+        void signin();       // Admin sign in
+        void addteacher();    // Add teacher
+        void addstudent();    // Add student
+        void editstudent();   // Edit student details
+        void editteacher();   // Edit teacher details
+        void viewstudent();   // View all students
+        void viewteacher();   // View all teachers
 };
