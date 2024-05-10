2e04c34d440b92d115c7decc38816b9f4e9e71a2
abdul1ah <ma2695633@gmail.com>
2024-05-11 02:48:26 +0500

    added add student function for admin

--- Database.cpp
+++ Database.cpp
@@ -227,5 +227,155 @@ void Admin::addteacher()
     cout << endl << "THE DATA OF TEACHER HAS BEEN ADDED" << endl;
     cout << "PRESS ANY KEY TO CONTINUE" << endl;
     cin>>f;
-} 
+}
+void Admin::addstudent()
+{
+    char f;
+    
+    string year, month, day;
+    cout << "PLEASE INPUT THE REGISTRATION DATE OF STUDENT" << endl;
+    cout << "ENTER YEAR    ";
+    cin >> year;
+    cout << "ENTER MONTH    ";
+    cin >> month;
+    cout << "ENTER DAY    ";
+    cin >> day;
+
+    while (stoi(year) < 1991 || stoi(year) > 2024 || stoi(month) < 1 || stoi(month) > 12 || stoi(day) < 1 || stoi(day) > 31) {
+        cout << "INVALID DATE!" <<endl;
+        cout << "RE-ENTER YEAR    ";
+        cin >> year;
+        cout << "RE-ENTER MONTH    ";
+        cin >> month;
+        cout << "RE-ENTER DAY    ";
+        cin >> day;
+    }
+
+    if (month.length() == 1) {
+        month = "0" + month;
+    }
+    if (day.length() == 1) {
+        day = "0" + day;
+    }
+    s_date = year + "/" + month + "/" + day;
+
+    cout << "PLEASE INPUT THE ID OF STUDENT E.G(595)   ";
+    cin >> s_id;
+    
+    while (stoi(s_id) < 1 || stoi(s_id) > 999) 
+    {
+        cout << "INVALID ID!" <<endl;
+        cout << "RE-ENTER ID   ";
+        cin >> s_id;
+    }
+    if (s_id.length() == 1 ){
+        s_id = "00" + s_id;
+    }
+    else if (s_id.length() == 2){
+        s_id = "0" + s_id;
+    }
+    s_id = year + s_id;
+
+    cout << "PLEASE INPUT SECTION OF STUDENT    ";
+    cin >> section;
+    
+    cout << "PLEASE INPUT THE FIRST NAME OF STUDENT    ";
+    cin >> s_firstname;
+    cout << "PLEASE INPUT THE LAST NAME OF STUDENT    ";
+    cin >> s_lastname;
+
+    cout << "PLEASE INPUT THE DEPARTMENT OF TEACHER; 1:FCSE|2:MGS|3:FME|4:FMCE    ";
+    cin >> s_department;
+    
+    while (stoi(s_department) < 1 || stoi(s_department) > 4 ){
+        cout << "INVALID CHOICE!" <<endl;
+        cout << "RE-ENETER DEPARTMENT    ";
+        cin >>s_department;
+    }
+    department x;
+    if (s_department == "1") {
+        x = FCSE;
+    }
+    else if (s_department == "2") {
+        x = MGS;
+    }
+    else if (s_department == "3") {
+        x = FME;
+    }
+    else if (s_department == "4") {
+        x = FMCE;
+    }
+
+    cout << "PLEASE SELECT THE GENDER OF TEACHER; 1:MALE|2:FEMALE    ";
+    cin >> s_gender;
+    while (stoi(s_gender) < 1 || stoi(s_gender) > 2){
+        cout << "INVALID CHOICE!" <<endl;
+        cout << "RE-ENETER GENDER    ";
+        cin >> s_gender;
+    }
+    
+    gender y;
+    if (s_gender == "1"){
+        y = male;
+    }
+    else if (s_gender == "2"){
+        y = female;
+    }
+
+    cout << "STUDENT USERNAME GENERATED    " << endl;
+    s_user_name = "u" + s_id + "@giki.edu,pk";
+    cout << "STUDENT PASSWORD GENERATED   " << endl;
+    s_password = year + month + day;
+
+    cout << "PLEASE INPUT THE PHONE NUMBER OF STUDENT    ";
+    cin >> s_number;
+    cout << "PLEASE INPUT THE CITY OF STUDENT AS ADDRESS    ";
+    cin >> s_address;
+    cout << "PLEASE INPUT THE FEE STATUS OF STUDENT    ";
+    cin >> fee;
+
+    int choice;
+
+    cout << "Enter 1 for Alevels, 2 for FSC, or 3 for Private: ";
+    cin >> choice;
+
+    while (choice < 1 || choice > 3) {
+        cout << "Invalid choice. Please enter 1, 2, or 3: ";
+        cin >> choice;
+    }
+
+    switch (choice) {
+        case 1:
+            s_qualification = "Alevels";
+            break;
+        case 2:
+            s_qualification = "FSC";
+            break;
+        case 3:
+            s_qualification = "Private";
+            break;
+    }
+
+    int no_of_subjects;
+    cout << "PLEASE INPUT NUMBER OF SUBJECTS    ";
+    cin >> no_of_subjects;
+
+    General::Marks student[no_of_subjects];
+
+    setSubject(student, no_of_subjects);
+
+    // Open file in append mode
+    ofstream out1("Student.txt", ios::app);  
+    ofstream out2("student_marks.txt", ios::app);
+    ofstream out3("student_data.txt", ios::app);
+    out1 << s_id << "," << section << "," << s_firstname << "," << s_lastname << "," << departments(x) << "," << s_user_name << "," << s_password << "," << s_date <<  ","  << genders(y) << "," << s_number << "," << s_address << "," << fee << "," << s_qualification << "," << getSubject(student, no_of_subjects) << endl;
+    out2 << getSubject(student, no_of_subjects) << endl;
+    out3 << s_id << "," << section << "," << getSubject(student, no_of_subjects) << endl;
+    out1.close();
+    out2.close();
+    out3.close();
+    cout << endl << "THE DATA OF STUDENT HAS BEEN ADDED" << endl;
+    cout << "PRESS ANY KEY TO CONTINUE" << endl;
+    cin>>f;
+}
 }
