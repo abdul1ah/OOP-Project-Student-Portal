c41e43589ddd198ec535f099daf3e0755e237b41
Saim123123 <167634098+Saim123123@users.noreply.github.com>
2024-05-12 13:06:53 +0500

    edited code

--- Database.cpp
+++ Database.cpp
@@ -1,231 +1,140 @@
-#include "Database.hpp"
-//--------------------Admin Class Functions--------------------
-void Admin::signin() {
+//--------------------Student Class Functions--------------------
+
+void Student::sign_in_student() {
     char f;
-    bool found = false;
-    ifstream file("Admin.txt");
+    ifstream file("Student.txt");
     if (!file.is_open()) {
         cout << "Error! Unable to open file.\n";
-        return; 
+        return;
     }
 
+    string firstName, lastName, id, dept, user, pass, date, gender, phone, address, fee, quali, subject, section;
+    string inputp, inputu;
     int tries = 0;
-    
-    string user,pass;
-    cout << "PLEASE INPUT THE USERNAME:    ";
-    cin >> username;
-    cout << "PLEASE INPUT THE PASSWORD:    ";
-    cin >> password;
 
-    while (getline(file, user, ',')) {
-            getline(file, pass);
-
-            if (username == user && password == pass) {
-                found = true;
-                break;
-            }
-        }
+     cout << "PLEASE INPUT YOUR USERNAME: ";
+    cin >> inputu;
+    cout << "PLEASE INPUT YOUR PASSWORD: ";
+    cin >> inputp;
 
     while (tries < 4) {
-        
-        if (found) {
+        bool found = false;
+        file.clear();
+        file.seekg(0);
 
-            cout << "THE USERNAME AND PASSWORD ARE CORRECT" << endl;
-            cout << "YOU CAN PROCEED" << endl;
-            cout << "PRESS ANY KEY TO CONTINUE" << endl;
-            cin >> f;
-            file.close();
+        while (getline(file, id, ',')) {
+            getline(file, section, ',');
+            getline(file, firstName, ',');
+            getline(file, lastName, ',');
+            getline(file, dept, ',');
+            getline(file, user, ',');
+            getline(file, pass, ',');
+            getline(file, date, ',');
+            getline(file, gender, ',');
+            getline(file, phone, ',');
+            getline(file, address, ',');
+            getline(file, fee, ',');
+            getline(file, quali, ',');
+            getline(file, subject);
 
-            char j = 'y';
+            if (user == inputu && pass == inputp) {
+                found = true;
+                cout << "THE USERNAME AND PASSWORD ARE CORRECT" << endl;
+                cout << "YOU CAN PROCEED" << endl;
+                cout << "PRESS ANY KEY TO CONTINUE" << endl;
+                cin>>f;
 
-            while (j != 8) {
+                // Call the appropriate functions to view attendance, marks, grades, registered courses, or fee status
+                // based on the user's input
+                char j = 'y';
+                while (j != 8) {
+                    system("clear");
+                    system("Color B5");
+                    cout << endl << "1.    PRESS 1 TO VIEW ATTENDANCE" << endl;
+                    cout << "2.    PRESS 2 TO VIEW MARKS" << endl;
+                    cout << "3.    PRESS BACKSPACE <------ TO EXIT STUDENT MODULE" << endl;
+                    cin>>j;
 
-                system("clear");
-                system("Color C7");
-                cout << endl << "1.    PRESS 1 TO ADD TEACHER" << endl;
-                cout << "2.    PRESS 2 TO ADD STUDENT" << endl;
-                cout << "3.    PRESS 3 TO EDIT TEACHER" << endl;
-                cout << "4.    PRESS 4 TO EDIT STUDENT" << endl;
-                cout << "5.    PRESS 5 TO VIEW ALL TEACHERS" << endl;
-                cout << "6.    PRESS 6 TO VIEW ALL STUDENTS" << endl;
-                cout << "7.    PRESS BACKSPACE <------ TO EXIT ADMIN MODULE" << endl;
-                cin>>j;
-
-                if (j == '1')
-                {
-                    addteacher();
+                    if (j == '1')
+                    {
+                        view_attendance();
+                    }
+                    else if (j == '2')
+                    {
+                        view_marks();
+                    }
+                    else if (j != 8)
+                    {
+                        cout << endl << "PLEASE PRESS VALID NUMBER" << endl;
+                    }
                 }
-                else if (j == '2')
-                {
-                    addstudent();
-                }
-                else if (j == '3')
-                {
-                    editteacher();
-                }
-                else if (j == '4')
-                {
-                    editstudent();
-                }
-                else if (j == '5')
-                {
-                    viewteacher();
-                }
-                else if (j == '6')
-                {
-                    viewstudent();
-                }
-                else if (j != 8)
-                {
-                    cout << endl << "PLEASE PRESS VALID NUMBER" << endl;
-                }
-
+                return;
             }
         }
 
-        else {
-            
+        if (!found) {
             cout << "USERNAME OR PASSWORD IS INCORRECT, TRY AGAIN! " << 3-tries << " TRIES REMAINING" << endl;
-            cout << "PLEASE INPUT THE USERNAME:    ";
-            cin >> username;
-            cout << "PLEASE INPUT THE PASSWORD:    ";
-            cin >> password;
-            file.clear();
-            file.seekg(0);
-
-            while (getline(file, user, ',')) {
-                getline(file, pass);
-
-                if (username == user && password == pass) {
-                    found = true;
-                    break;
-                }
-            }
-            tries ++;
-        }
-
-        if (tries == 3 && !found){
-
-            cout << endl << "YOU HAVE EXCEEDED THE NUMBER OF TRIES, PLEASE TRY AGAIN LATER" << endl;
-            sleep(5);
-            return;
+            cout << "PLEASE INPUT RENTER: " << endl;
+            cin >> inputu;
+            cin >> inputp;
+            tries++;
         }
     }
 
-void Admin::addteacher()
+    cout << endl << "YOU HAVE EXCEEDED THE NUMBER OF TRIES, PLEASE TRY AGAIN LATER" << endl;
+    sleep(5);
+    return;
+}
+void Student::view_attendance() {
+    string regNumber, section, subject, date, attendanceStatus, regNumberFromFile;
+    cout << "Enter your registration number: ";
+    cin >> regNumber;
+
+    ifstream file("student_marks.txt");
+    if (!file.is_open()) {
+        cout << "Error opening file!" << endl;
+        return;
+    }
+
+    bool found = false;
+    while (file >> subject >> section >> regNumberFromFile >> date >> attendanceStatus) {
+        if (regNumber == regNumberFromFile) {
+            cout << "Subject: " << subject << ", Section: " << section << ", Date: " << date << ", Attendance: " << attendanceStatus << endl;
+            found = true;
+        }
+    }
+
+    if (!found) {
+        cout << "No attendance records found for registration number: " << regNumber << endl;
+    }
+    sleep(4);
+
+    file.close();
+}
+void Student::view_marks()
 {
-    char f;
-    
-    string year, month, day;
-    cout << "PLEASE ENTER THE REGISTRATION DATE OF TEACHER   " << endl;
-    cout << "ENTER YEAR    ";
-    cin >> year;
-    cout << "ENTER MONTH    ";
-    cin >> month;
-    cout << "ENTER DAY    ";
-    cin >> day;
+    string regNumber, section, subject, date, attendanceStatus, regNumberFromFile;
+    cout << "Enter your registration number: ";
+    cin >> regNumber;
 
-    while (stoi(year) < 1991 || stoi(year) > 2024 || stoi(month) < 1 || stoi(month) > 12 || stoi(day) < 1 || stoi(day) > 31) {
-        cout << "INVALID DATE!" <<endl;
-        cout << "RE-ENTER YEAR    ";
-        cin >> year;
-        cout << "RE-ENTER MONTH    ";
-        cin >> month;
-        cout << "RE-ENTER DAY    ";
-        cin >> day;
+    ifstream file("attendance.txt");
+    if (!file.is_open()) {
+        cout << "Error opening file!" << endl;
+        return;
     }
 
-    if (month.length() == 1) {
-        month = "0" + month;
-    }
-    if (day.length() == 1) {
-        day = "0" + day;
-    }
-    t_date = year + "/" + month + "/" + day;
-
-    cout << "PLEASE INPUT THE ID OF TEACHER e.g(503)   ";
-    cin >> t_id;
-
-    while (stoi(t_id) < 1 || stoi(t_id) > 9999){
-        cout << "INVALID ID!" <<endl;
-        cout << "RE-ENTER ID    ";
-        cin >> t_id;
+    bool found = false;
+    while (file >> subject >> section >> regNumberFromFile >> date >> attendanceStatus) {
+        if (regNumber == regNumberFromFile) {
+            cout << "Subject: " << subject << ", Section: " << section << ", Date: " << date << ", Attendance: " << attendanceStatus << endl;
+            found = true;
+        }
     }
 
-    if (t_id.length() == 1){
-        t_id = "00" + t_id;
+    if (!found) {
+        cout << "No attendance records found for registration number: " << regNumber << endl;
     }
-    else if (t_id.length() == 2) {
-        t_id = "0" + t_id;
-    }
+    sleep(4);
 
-    t_id = year + t_id;
-
-    cout << "INPUT SECTION OF TEACHER    ";
-    cin >> section;
-    
-    cout << "PLEASE INPUT THE FIRST NAME OF TEACHER    ";
-    cin >> t_firstname;
-    cout << "PLEASE INPUT THE LAST NAME OF TEACHER    ";
-    cin >> t_lastname;
-
-    cout << "PLEASE INPUT THE DEPARTMENT OF TEACHER; 1:FCSE|2:MGS|3:FME|4:FMCE    ";
-    cin >> t_department;
-    
-    while (stoi(t_department) < 1 || stoi(t_department) > 4 ){
-        cout << "INVALID CHOICE!" <<endl;
-        cout << "RE-ENETER DEPARTMENT    ";
-        cin >> t_department;
-    }
-    department x;
-    if (t_department == "1") {
-        x = FCSE;
-    }
-    else if (t_department == "2") {
-        x = MGS;
-    }
-    else if (t_department == "3") {
-        x = FME;
-    }
-    else if (t_department == "4") {
-        x = FMCE;
-    } 
-
-    cout << "PLEASE INPUT THE SUBJECT OF TEACHER    ";
-    cin >> subject;
-
-    cout << "PLEASE SELECT THE GENDER OF TEACHER; 1:MALE|2:FEMALE    ";
-    cin >> t_gender;
-    while (stoi(t_gender) < 1 || stoi(t_gender) > 2){
-        cout << "INVALID CHOICE!" <<endl;
-        cout << "RE-ENETER GENDER    ";
-        cin >> t_gender;
-    }
-    
-    gender y;
-    if (t_gender == "1"){
-        y = male;
-    }
-    else if (t_gender == "2"){
-        y = female;
-    }
-
-    cout << "TEACHERS USERNAME GENERATED   " << endl;
-    t_username = "t" + t_id + "@giki.edu.pk" ;
-    cout << "TEACHERS PASSWORD GENERATED    " << endl;
-    t_password = year + month + day;
-
-    cout << "PLEASE INPUT THE NUMBER OF TEACHER    ";
-    cin >> t_number;
-    cout << "PLEASE INPUT THE QUALIFICATION OF TEACHER    ";
-    cin >> t_qualification;
-    cout << "PLEASE INPUT THE CITY OF TEACHER AS ADDRESS    ";
-    cin >> address;
-    ofstream out("Teacher.txt", ios::app);  // Open file in append mode
-    out << t_id << "," << section << "," << t_firstname << "," << t_lastname << "," << departments(x) << "," <<subject << "," << t_username << "," << t_password << "," << t_date << "," << genders(y) << "," << t_number << "," << t_qualification << "," << address << endl;
-    out.close();  // Remember to close the file
-    cout << endl << "THE DATA OF TEACHER HAS BEEN ADDED" << endl;
-    cout << "PRESS ANY KEY TO CONTINUE" << endl;
-    cin>>f;
-} 
+    file.close();
 }
