9d942e89e1fb518a14802b4a39e78f28765a239a
Saim123123 <167634098+Saim123123@users.noreply.github.com>
2024-05-12 14:10:40 +0500

    Update Source.cpp
    corrected the code

--- Source.cpp
+++ Source.cpp
@@ -1,89 +1,41 @@
-#pragma once
-
-#include<iostream>
-#include<string>
-#include<unistd.h>
-#include<fstream>
-#include<sstream>
-
-using namespace std;
-
-//general class
-class General
+#include "Database.hpp"
+//#include "Database.cpp"
+#include "Untitled-1.cpp"
+int main()
 {
-    protected:
-    struct Marks{
-        string s_marks;
-        string s_subject;
-    };
+    char i=0;
+    while (i != 27)
+    {
+        system("clear");
+        
+        cout << endl << endl << "\t\tLEARNING MANAGEMENT SYSTEM" << endl;
+        cout << endl << "1.    PRESS 1 TO LOGIN AS ADMIN" << endl;
+        cout << "2.    PRESS 2 TO LOGIN AS TEACHER" << endl;
+        cout << "3.    PRESS 3 TO LOGIN AS STUDENT" << endl;
+        cout << "5.    PRESS Esc TO END PROGRAM" << endl;
+        cin>>i;
+        if (i == '1')
+        {
+            Admin aobj;
+            aobj.signin();
+        }
+        else if (i == '2')
+        {
+            Teacher tobj;
+            tobj.signin();
+        }
+        else if (i == '3')
+        {
+            Student sobj;
+            sobj.sign_in_student();
+        }
+        else if (i != 27)
+        {
+            system("clear");
+            cout << endl << "PLEASE PRESS A VALID NUMBER" << endl;
+        }
+    }
+    return 0;
+}
 
-    enum department{FCSE, MGS, FME, FMCE};
-    string departments(department);
 
-    enum gender { male, female };
-    string genders(gender);
-    
-    void setSubject(Marks [], int);
-
-    string getSubject(Marks [], int);
-
-};
-
-// Class declaration for Admin
-class Admin:public General
-{
-protected:
-    string username;  // Admin username
-    string password;  // Admin password
-    
-    string ID;
-    string FirstName;
-    string LastName;
-    string Department;
-    string UserName;
-    string Password;
-    string Date;
-    string Gender;
-    string Number;
-    string Qualification;
-    string address;
-    string Course;
-    string Marks;
-    string fee;
-    string section;
-    string subject;
-
-public:
-    // Function declarations for Admin operations
-    void signin();       // Admin sign in
-    void addteacher();    // Add teacher
-    void addstudent();    // Add student
-    void editstudent();   // Edit student details
-    void editteacher();   // Edit teacher details
-    void viewstudent();   // View all students
-    void viewteacher();   // View all teachers
-};
-
-// Class declaration for Student
-class Student:public Admin
-{
-private:
-    // Student attributes
-public:
-    // Function declarations for Student operations
-    void sign_in_student();             // Student sign in
-    void view_attendance();             // View attendance
-    void view_marks();                  // View marks
-};
-
-// Derived class declaration for Teacher inheriting Admin
-class Teacher :public Admin
-{
-public:
-    // Function declarations for Teacher operations
-    void signin();           // Teacher sign in
-    void timetable();        // View timetable
-    void markingattendance();  // Mark attendance
-    void assignmarks();      // Assign marks
-   // Assign grades
-};
