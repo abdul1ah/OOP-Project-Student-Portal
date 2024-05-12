a1361064e26aabab5bd7ebef028d15d86c336cdf
Moosa-7 <168295763+Moosa-7@users.noreply.github.com>
2024-05-12 14:07:12 +0500

    Update Database.hpp

--- Database.hpp
+++ Database.hpp
@@ -8,57 +8,82 @@
 
 using namespace std;
 
-// Class declaration for Admin
-class Admin
+//general class
+class General
 {
     protected:
-        string username;  // Admin username
-        string password;  // Admin password
-
-    private:
-        // Teacher attributes
-        string t_id;
-        string t_firstname;
-        string t_lastname;
-        string t_department;
-        string tuser_name;
-        string tpassword;
-        string t_date;
-        string t_gender;
-        string t_number;
-        string t_qualification;
-        string address;
-            struct Marks
-        {
-            string s_marks;
-            string s_subject;
-        };
-
-        // Student attributes
-        string suser_name,spassword;
-        string s_id;
-        string s_firstname;
-        string s_lastname;
-        string s_department;
-        string s_user_name;
-        string s_password;
-        string s_date;
-        string s_gender;
-        string s_number;
-        string s_address;
-        string fee;
-        string s_qualification;
+    struct Marks{
         string s_marks;
-        void setMarks(Marks [], int);
-        string getMarks(Marks [], int);
+        string s_subject;
+    };
 
-    public:
-        // Function declarations for Admin operations
-        void signin();       // Admin sign in
-        void addteacher();    // Add teacher
-        void addstudent();    // Add student
-        void editstudent();   // Edit student details
-        void editteacher();   // Edit teacher details
-        void viewstudent();   // View all students
-        void viewteacher();   // View all teachers
+    enum department{FCSE, MGS, FME, FMCE};
+    string departments(department);
+
+    enum gender { male, female };
+    string genders(gender);
+    
+    void setSubject(Marks [], int);
+
+    string getSubject(Marks [], int);
+
+};
+
+// Class declaration for Admin
+class Admin:public General
+{
+protected:
+    string username;  // Admin username
+    string password;  // Admin password
+    
+    string ID;
+    string FirstName;
+    string LastName;
+    string Department;
+    string UserName;
+    string Password;
+    string Date;
+    string Gender;
+    string Number;
+    string Qualification;
+    string address;
+    string Course;
+    string Marks;
+    string fee;
+    string section;
+    string subject;
+
+public:
+    // Function declarations for Admin operations
+    void signin();       // Admin sign in
+    void addteacher();    // Add teacher
+    void addstudent();    // Add student
+    void editstudent();   // Edit student details
+    void editteacher();   // Edit teacher details
+    void viewstudent();   // View all students
+    void viewteacher();   // View all teachers
+};
+
+// Class declaration for Student
+class Student:public Admin
+{
+private:
+    // Student attributes
+public:
+    // Function declarations for Student operations
+    void sign_in_student();             // Student sign in
+    void view_attendance();             // View attendance
+    void view_marks();                  // View marks
+};
+
+// Derived class declaration for Teacher inheriting Admin
+class Teacher :public Admin
+{
+public:
+    // Function declarations for Teacher operations
+    void signin();           // Teacher sign in
+    void timetable();        // View timetable
+    void markingattendance();  // Mark attendance
+    void assignmarks();      // Assign marks
+   // Assign grades
 };
