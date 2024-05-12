d141c3c83189c5955957284e975bf453104e5c31
Saim123123 <167634098+Saim123123@users.noreply.github.com>
2024-05-12 13:14:57 +0500

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,54 @@
+//TO FIRST TIME RUN THE PROGRAM TO TEST:
+//
+//FOR STUDENT:-
+//USERNAME: GORDON123
+//PASSWORD: JLJIJJIJJ345
+//
+//FOR TEACHER:-
+//USERNAME: HAPO1234
+//PASSWORD: PASS1234
+//
+//FOR ADMIN:-
+//USERNAME: Admin
+//PASSWORD: Admin123
+
+#include "Database.hpp"
+//#include "Database.cpp"
+#include "Untitled-1.cpp"
+int main()
+{
+    char i=0;
+    while (i != 27)
+    {
+        system("clear");
+        
+        cout << endl << endl << "\t\tLEARNING MANAGEMENT SYSTEM" << endl;
+        cout << endl << "1.    PRESS 1 TO LOGIN AS ADMIN" << endl;
+        cout << "2.    PRESS 2 TO LOGIN AS TEACHER" << endl;
+        cout << "3.    PRESS 3 TO LOGIN AS STUDENT" << endl;
+        cout << "5.    PRESS Esc TO END PROGRAM" << endl;
+        cin>>i;
+        if (i == '1')
+        {
+            Admin aobj;
+            aobj.signin();
+        }
+        else if (i == '2')
+        {
+            Teacher tobj;
+            tobj.signin();
+        }
+        else if (i == '3')
+        {
+            Student sobj;
+            sobj.sign_in_student();
+        }
+        else if (i != 27)
+        {
+            system("clear");
+            cout << endl << "PLEASE PRESS A VALID NUMBER" << endl;
+        }
+    }
+    return 0;
+}
+
